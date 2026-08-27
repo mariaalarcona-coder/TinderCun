ff7ef5c7f1445746a14f69f08a087bba9872e85c
Sala G 404 <salag404@cunadm.edu.local>
2026-08-26 20:27:44 -0500

    v2

--- tinderCUN.py
+++ tinderCUN.py
@@ -1,3 +1,40 @@
+def registrarPersonas():
+    individuo={}
+    nombre=input("Ingresa tu nombre: ")
+    individuo["Nombre"]=nombre
+    edad=int(input("Ingresa tu edad: "))
+    individuo["Edad"]=edad
+    ciudad=input("Ingresa tu ciudad: ")
+    individuo["Ciudad"]=ciudad
+    genero=input("Ingresa tu género: ")
+    individuo["Genero"]=genero
+    generoInteres=input("Ingresa el género de interés: ")
+    individuo["Genero Interesado"]=generoInteres
+    edadMinima=int(input("Ingresa la edad mínima: "))
+    individuo["Edad Minima"]=edadMinima
+    edadMaxima=int(input("Ingresa la edad máxima: "))
+    individuo["Edad Máxima"]=edadMaxima
+    intereses=input("Ingresa tus interese(Hoobies): ")
+    individuo["Hooby"]=intereses
+    distanciaMaxima=int(input("Ingresa una distancia máxima: "))
+    individuo["Distancia Máxima"]=distanciaMaxima
+    print(individuo)
+    return individuo
+
+def mostrarPersonas(personas):
+    print(personas)
+
 def main():
-    print("Hola mundo")
-    main()
+    cuantasPersonas=int(input("Ingresa cuantas personas vas a registrar"))
+    #Registrando Personas
+    personas={}
+    for i in range (0,cuantasPersonas):
+        print("i",i)
+        print(personas)
+        personas[i]= registrarPersonas()
+    #Mostrar Personas
+    mostrarPersonas(personas)
+
+    print()
+
+main()
